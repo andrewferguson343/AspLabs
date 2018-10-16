6286ac0bc90d3c0fc164a845aef6a2c2af928e3e
Andrew Ferguson <you@example.com>
2018-10-16 10:10:14 -0400

    added html files

---
+++ lab6/calc.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="calc.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+  <asp:Label ID="Label1" runat="server" Text="Number 1:"></asp:Label>
+  <asp:TextBox ID="Number1" runat="server"></asp:TextBox>
+  <br /><br/>
+  <asp:Label ID="Label2" runat="server" Text="Label">Number 2:</asp:Label>
+  <asp:TextBox ID="Number2" runat="server"></asp:TextBox>
+  <br /><br/>
+  <asp:Button ID="ButtonAdd" runat="server" Text="Add"  OnClick="ButtonAdd_Click"/>
+  <asp:Button ID="ButtonSubtract" runat="server" Text="Subtract" OnClick="ButtonSubtract_Click" />
+  <asp:Button ID="ButtonDivide" runat="server" Text="Divide"  OnClick="ButtonDivide_Click"/>
+  <asp:Button ID="ButtonMultiply" runat="server" Text="Multiply" OnClick="ButtonMultiply_Click" />
+
+
+  <p id="answer" runat="server">Result</p>
+</asp:Content>
+
---
+++ lab6/form.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+  protected void Page_Load(object sender, EventArgs e)
+  {
+
+  }
+
+  protected void Button1_Click(object sender, EventArgs e)
+  {
+    string text1 = FirstNameText.Text;
+    Response.Write("<p>First Name:"+ text1 +"</p> <br/>");
+
+    string text2 = LastNameText.Text;
+    Response.Write("<p>Last Name:" + text2 + "</p> <br/>");
+
+    string text3 = ZipText.Text;
+    Response.Write("State" + text3);
+
+    string text5 = AddressText.Text;
+    Response.Write("<p>CAddress:" + text5 + "</p> <br/>");
+
+    string text6 = CheckBox1.Checked ? "Checked" : "Not Checked";
+    Response.Write("<p>Checkbox:" + text6 + "</p> <br/>");
+
+    string text4 = RadioButtonList1.SelectedValue;
+    Response.Write(RadioButtonList1.SelectedValue);
+
+    
+  }
+
+  protected void TextBox1_TextChanged(object sender, EventArgs e)
+  {
+   
+  }
+
+  protected void LastNameText_TextChanged(object sender, EventArgs e)
+  {
+   
+
+  }
+
+  protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+  {
+    
+  }
+
+  protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+  {
+
+  }
+}
